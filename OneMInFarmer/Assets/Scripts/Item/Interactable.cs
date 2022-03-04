224e9a01c904167ef3b9a9c3830ca940618b2e7d
Art <jakkrapan.art@mail.kmutt.ac.th>
2022-03-04 21:45:12 +0700

    [Create] Interactable script

---
+++ OneMInFarmer/Assets/Scripts/Item/Interactable.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] private UnityEvent interactEvent;
+    private SpriteRenderer sr;
+
+    private Color defaultColor;
+    [SerializeField] private Color highlightColor;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void Interact()
+    {
+        interactEvent?.Invoke();
+    }
+
+    public void ShowObjectHighlight()
+    {
+        sr.color = highlightColor;
+    }
+
+    public void HideObjectHighlight()
+    {
+        sr.color = defaultColor;
+    }
+}
