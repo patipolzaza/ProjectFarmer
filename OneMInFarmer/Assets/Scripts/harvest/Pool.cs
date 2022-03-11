bb72e697c49ebc6754caa26fb908ba46eba008ad
Kluay <patipol.1456@mail.kmutt.ac.th>
2022-03-11 19:13:35 +0700

    Add pool.cs

---
+++ OneMInFarmer/Assets/Scripts/harvest/Pool.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool : Interactable
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+    }
+}
