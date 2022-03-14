a6617237f65e466ae34cc1a3e28b2ab5b46ccbe6
Art <jakkrapan.art@mail.kmutt.ac.th>
2022-03-14 10:30:33 +0700

    [Update] Implement functions for wallet

--- OneMInFarmer/Assets/Scripts/Player/Wallet.cs
+++ OneMInFarmer/Assets/Scripts/Player/Wallet.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wallet : MonoBehaviour
+public class Wallet
 {
-    // Start is called before the first frame update
-    void Start()
+    public int currentCoin { get; private set; }
+
+    public Wallet(int initialCoin)
     {
-        
+        currentCoin = initialCoin;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EarnCoin(int amount)
     {
-        
+        currentCoin += amount;
+    }
+
+    public void LoseCoin(int amount)
+    {
+        if (currentCoin <= amount)
+        {
+            currentCoin = 0;
+        }
+        else
+        {
+            currentCoin -= amount;
+        }
     }
 }
