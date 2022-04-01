3f0f6e43e22b615db80dbaa288d2e15052b3fec7
Art <jakkrapan.art@mail.kmutt.ac.th>
2022-04-02 02:02:14 +0700

    [Edit] Add description for IValuable

--- OneMInFarmer/Assets/Scripts/Interfaces/IValuable.cs
+++ OneMInFarmer/Assets/Scripts/Interfaces/IValuable.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public interface IValuable
 {
-    public bool Purchase();
-    public void Sell();
+    /// <summary>
+    /// Purchase valuable object from shop.
+    /// </summary>
+    /// <returns>True if success purchase, False if not.</returns>
+    public bool Purchase(); // Return bool that is finished purchase.
+    /// <summary>
+    /// Function for sell valuable object
+    /// </summary>
+    /// <returns>Price that valuable object was sold.</returns>
+    public int Sell();
     public GameObject GetObject();
 }
