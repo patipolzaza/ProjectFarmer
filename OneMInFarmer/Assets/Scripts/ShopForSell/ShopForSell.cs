71dc80950ead7879c08f05c15c7411f25a45e3c2
Kluay <patipol.1456@mail.kmutt.ac.th>
2022-04-06 00:00:46 +0700

    [Update] ShopForSell.cs เพิ่มTMPro เพื่อใช้แสดงราคาที่จะขายของ

--- OneMInFarmer/Assets/Scripts/ShopForSell/ShopForSell.cs
+++ OneMInFarmer/Assets/Scripts/ShopForSell/ShopForSell.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class ShopForSell : Interactable
 {
     public static ShopForSell Instance { get; private set; }
     private ItemContainer itemContainer;
     public bool isFinishSellingProcess { get; private set; } = false;
     public int totalSoldPrice { get; private set; }
+    [SerializeField] private TextMeshProUGUI priceTMPro;
 
     protected override void Awake()
     {
@@ -62,5 +63,29 @@ public class ShopForSell : Interactable
     public void ResetTotalSoldPrice()
     {
         totalSoldPrice = 0;
+    }
+
+    public void ShowPrice(int price)
+    {
+        priceTMPro.text = price.ToString();
+    }
+    public override void HideObjectHighlight()
+    {
+        sr.color = defaultColor;
+        priceTMPro.text = null;
+        priceTMPro.gameObject.SetActive(false);
+    }
+    public override void ShowObjectHighlight()
+    {
+        if (isInteractable)
+        {
+            sr.color = highlightColor;
+            priceTMPro.gameObject.SetActive(true);
+        }
+        else
+        {
+            sr.color = defaultColor;
+            priceTMPro.gameObject.SetActive(false);
+        }
     }
 }
