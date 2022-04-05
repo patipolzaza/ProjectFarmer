ea0cf147e161eba13f77e04f9e122ecef4668470
Art <jakkrapan.art@mail.kmutt.ac.th>
2022-04-06 01:59:41 +0700

    [Remove] Container and SellAllItem function from ShopForSell

--- OneMInFarmer/Assets/Scripts/ShopForSell/ShopForSell.cs
+++ OneMInFarmer/Assets/Scripts/ShopForSell/ShopForSell.cs
@@ -5,7 +5,6 @@ using TMPro;
 public class ShopForSell : Interactable
 {
     public static ShopForSell Instance { get; private set; }
-    private ItemContainer itemContainer;
     public bool isFinishSellingProcess { get; private set; } = false;
     public int totalSoldPrice { get; private set; }
     [SerializeField] private TextMeshProUGUI priceTMPro;
@@ -13,51 +12,18 @@ public class ShopForSell : Interactable
     protected override void Awake()
     {
         base.Awake();
-        itemContainer = new ItemContainer();
 
         Instance = this;
     }
 
     public bool PutItemInContainer(IValuable valuable)
     {
-        Debug.Log("Selling");
         if (valuable != null)
         {
-            valuable.Sell();
-   
-            //itemContainer.Put(valuable);
-            //pickableObject.SetActive(false);
+            totalSoldPrice += valuable.Sell();
         }
 
         return true;
-    }
-
-    public void SellAllItemsInContainer()
-    {
-        isFinishSellingProcess = false;
-        SellItemInContainerProcess();
-    }
-
-    private void SellItemInContainerProcess()
-    {
-        int loopCount = itemContainer.GetItemCount;
-
-        for (int i = 0; i < loopCount; i++)
-        {
-            IValuable valuableObj = itemContainer.PickItemInStash;
-            if (valuableObj != null)
-            {
-
-                int soldPrice = valuableObj.Sell();
-                totalSoldPrice += soldPrice;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        isFinishSellingProcess = true;
     }
 
     public void ResetTotalSoldPrice()
