b54aa61c00fa0b7e2e7b7e05587bfafb418319a8
Art <jakkrapan.art@mail.kmutt.ac.th>
2022-04-14 14:46:13 +0700

    [Delete] System.serializable from State and StateMachine

--- OneMInFarmer/Assets/Scripts/StateMachine/StateMachine.cs
+++ OneMInFarmer/Assets/Scripts/StateMachine/StateMachine.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
 public class StateMachine
 {
     public State currentState { get; private set; }
