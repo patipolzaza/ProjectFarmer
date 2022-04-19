88910d152e27bc12b8b6d8833922c3a5c2e04446
Art <jakkrapan.art@mail.kmutt.ac.th>
2022-04-19 21:32:15 +0700

    [Update] ProductShowcase script

--- OneMInFarmer/Assets/Scripts/ShopBuy/ProductShowcase.cs
+++ OneMInFarmer/Assets/Scripts/ShopBuy/ProductShowcase.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProductShowcase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image _productImage;
+
+    public void UpdateProductShowcase(IBuyable buyable)
     {
-        
+        if (buyable != null)
+        {
+            SetProductImage(buyable.GetIcon);
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void SetProductImage(Sprite productIcon)
     {
-        
+        _productImage.sprite = productIcon;
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
     }
 }
