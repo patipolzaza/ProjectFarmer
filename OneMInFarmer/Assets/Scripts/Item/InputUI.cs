d853363858215bb4c2f4c160d4c58e9976744a1b
Art <jakkrapan.art@mail.kmutt.ac.th>
2022-04-29 23:14:51 +0700

    [Edit] Remove Show,Hide pick input ui functions

--- OneMInFarmer/Assets/Scripts/Item/InputUI.cs
+++ OneMInFarmer/Assets/Scripts/Item/InputUI.cs
@@ -5,22 +5,11 @@ using UnityEngine;
 public class InputUI : WindowUIBase
 {
     public static InputUI Instance { get; private set; }
-    [SerializeField] private GameObject _pickInputUI;
     [SerializeField] private GameObject _dropInputUI;
 
     private void Awake()
     {
         Instance = this;
-    }
-
-    public void ShowPickInputUI()
-    {
-        _pickInputUI.SetActive(true);
-    }
-
-    public void HidePickInputUI()
-    {
-        _pickInputUI.SetActive(false);
     }
 
     public void ShowDropInputUI()
